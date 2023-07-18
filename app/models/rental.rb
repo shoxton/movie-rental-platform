5359354199bcbb826fc4bfb038436173c4179934
André Pereira <andrepereirapink@gmail.com>
2023-07-18 16:29:08 -0300

    add uniqueness validation

--- app/models/rental.rb
+++ app/models/rental.rb
@@ -1,4 +1,5 @@
 class Rental < ApplicationRecord
+    validates :user_id, uniqueness: {scope: :movie_id, message: 'has already rented this movie'}
     belongs_to :user
     belongs_to :movie
-  end
+end
